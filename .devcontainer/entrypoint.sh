b6f62955c58001264b92c69a46f69f7fd45c30f9
Nopsled <metas.sided.00@icloud.com>
2025-01-10 02:47:49 +0000

    add

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -4,39 +4,30 @@
 # Function to handle shutdown
 cleanup() {
     echo "Shutdown signal received. Executing cleanup..."
-    # Release the zrok tunnel
-    #zrok release "$(basename "$PWD")"
+    zrok release "$(basename "$PWD")"
     zrok disable # disable release also the name?
     echo "Cleanup completed. Exiting."
-    exit 0
 }
 
 # Trap SIGTERM and SIGINT signals
-trap cleanup SIGTERM SIGINT
+trap cleanup SIGTERM SIGINT SIGKILL
 
 # Load environment variables from .env if it exists
-if [ -f .env ]; then
-    export $(grep -v '^#' .env | xargs)
-fi
-
-# Export UNIQUE_NAME for use in server.ts
-export UNIQUE_NAME="$(basename "$PWD")"
-
-# Enable zrok
-echo "Enabling zrok..."
-zrok enable "$ZROK_TOKEN"
-ENABLE_STATUS=$?
-
-if [ $ENABLE_STATUS -ne 0 ]; then
-    echo "Failed to enable zrok. Exiting."
-    exit 1
-fi
-echo "zrok enabled successfully."
+#if [ -f .env ]; then
+#    export $(grep -v '^#' .env | xargs)
+#fi
 
 # Start the Deno server in the background
 deno run --allow-all --watch server.ts &
 DENOPID=$!
 echo "Deno server started"
+
+# Export UNIQUE_NAME for use in server.ts
+export UNIQUE_NAME="$(basename "$PWD")"
+
+# Enable zrok
+zrok enable "$ZROK_TOKEN"
+echo "zrok enabled successfully."
 
 # Reserve the zrok tunnel in the background
 zrok reserve public localhost:3001 --unique-name "$(basename "$PWD")" --backend-mode proxy &
